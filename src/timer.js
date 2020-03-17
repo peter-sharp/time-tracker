3c4b4dadc9782a0ac2616fa55a6d2f48e72f3a0b
Peter Sharp <peter.victor.sharp@gmail.com>
2020-03-17 23:16:16 +1300

    Adds onStop/onStart events

--- src/timer.js
+++ src/timer.js
@@ -1,16 +1,16 @@
 const getId = ((i = 1) => () => (i += 1))();
-export const Timer = ({ onStop }) => {
+export const Timer = ({ onStop, onStart }) => {
   const name = "timer_controls_" + getId();
   const idStart = name + "_start";
   const idEnd = name + "_end";
   return (
     <section class="timer">
       <div class="radio-buttons radio-buttons--timer">
-        <input type="radio" id={idStart} name={name} />
+        <input type="radio" id={idStart} name={name} onclick={onStart}/>
         <label for={idStart} class="timer__button">
           Start
         </label>
-        <input type="radio" id={idEnd} name={name} />
+        <input type="radio" id={idEnd} name={name} onclick={onStop}/>
         <label for={idEnd} class="timer__button">
           Stop
         </label>
